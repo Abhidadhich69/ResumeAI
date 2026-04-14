ea437b6a60e920d02b6ae7584c7fd4793d97de03
Abhidadhich69 <sharmaabhi72005@gmail.com>
2026-04-14 08:57:26 +0530

    commited

--- Frontend/src/features/auth/services/auth.api.js
+++ Frontend/src/features/auth/services/auth.api.js
@@ -2,7 +2,7 @@ import axios from "axios"
 
 
 const api = axios.create({
-    baseURL: ["http://localhost:3000","https://resumeai-ee3z.onrender.com"],
+    baseURL: "https://resumeai-ee3z.onrender.com",
     withCredentials: true
 })
 
--- Frontend/src/features/interview/services/interview.api.js
+++ Frontend/src/features/interview/services/interview.api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL:[ "http://localhost:3000","https://resumeai-ee3z.onrender.com"],
+    baseURL:"https://resumeai-ee3z.onrender.com",
     withCredentials: true,
 })
 
